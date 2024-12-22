ebc5efc69ada7031c095fc9a1d464f221e334f13
Ilya Averyanov <av@rubybox.dev>
2024-12-22 21:50:36 +0200

    Remove unnecessary tests
    For cases now checked with type-checker

--- test/pdu/errors_test.exs
+++ test/pdu/errors_test.exs
@@ -5,12 +5,6 @@ defmodule SMPPEX.Pdu.ErrorsTest do
 
   doctest SMPPEX.Pdu.Errors
 
-  test "code_by_name" do
-    assert_raise FunctionClauseError, fn ->
-      Errors.code_by_name(:NON_EXISTING_ERROR_CODE)
-    end
-  end
-
   test "format" do
     assert_raise FunctionClauseError, fn ->
       Errors.format(:not_an_integer)
