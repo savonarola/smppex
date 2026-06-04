615375e81c99b1b47b2f683e8cbdbab5cfb81127
Ilia Averianov <av@rubybox.dev>
2026-06-04 12:02:06 +0300

    update erlang, elixir and deps

--- mix.exs
+++ mix.exs
@@ -13,17 +13,22 @@ defmodule Smppex.Mixfile do
       description: description(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ],
       package: package(),
       dialyzer: [
         plt_add_apps: [:ssl]
       ],
       docs: docs()
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
